d1029eec63d85132e64bb5c711fc531dd42c76e7
ingenarel <ingenarelitems@gmail.com>
2024-07-10 00:01:30 +0600

    f

--- lua/_plugins_/_load-plugins_.lua
+++ lua/_plugins_/_load-plugins_.lua
@@ -10,6 +10,9 @@ Plug("windwp/nvim-autopairs")
 Plug("nvim-lua/plenary.nvim")
 Plug("nvim-telescope/telescope.nvim")
 Plug("lukas-reineke/indent-blankline.nvim")
+Plug("neovim/nvim-lspconfig")
+Plug("williamboman/mason-lspconfig.nvim")
+Plug("williamboman/mason.nvim")
 vim.call("plug#end")
 
 -- enable more colors
@@ -19,11 +22,11 @@ vim.call("plug#end")
 require("nvim-tree").setup()
 require("colorizer").setup()
 require("telescope").setup()
-require("ibl").setup()
 
 require("_plugins_._lualine-config_")
 require("_plugins_._autopairs-config_")
 require("_plugins_._nvim-tree-config_")
 require("_plugins_._dashboard-config_")
-
+require("_plugins_._mason-config_")
+require("_plugins_._indent-line-config_")
 
