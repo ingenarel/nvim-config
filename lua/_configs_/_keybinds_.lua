41a081d8f9ae7d8b466028093ef2abca947b00c6
ingenarel <ingenarelitems@gmail.com>
2024-07-10 05:53:20 +0600

    stopped fucking around and finally figured shit out.

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeToggle<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<F3>', ':Telescope find_files<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<S-F3>', ':Telescope<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F15>', ':Telescope<CR>', {noremap = true}) -- shift f3 is f15
 vim.api.nvim_set_keymap('n', '<F4>', ':LazyGit<CR>', {noremap = true})
