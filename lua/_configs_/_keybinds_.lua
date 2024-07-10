a49386252556eee68f492f835ad4b4e7099cd1fb
ingenarel <ingenarelitems@gmail.com>
2024-07-11 03:08:18 +0600

    f1 now brings up dashboard and ctrl+f1 that means the f25 key brings up the help menu.

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeToggle<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<F3>', ':Telescope find_files<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F15>', ':Telescope<CR>', {noremap = true}) -- shift f3 is f15
+vim.api.nvim_set_keymap('n', '<F15>', ':Telescope<CR>', {noremap = true}) -- shift f3 is <F15>
 vim.api.nvim_set_keymap('n', '<F4>', ':LazyGit<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<F25>', ':help<CR>', {noremap = true}) -- ctrl f1 is <F25>
+vim.api.nvim_set_keymap('n', '<F1>', ':Dashboard<CR>', {noremap = true})
 
