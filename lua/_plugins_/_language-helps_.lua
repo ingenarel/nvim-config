0cfbe52ae9fb764353ea0931b6353dcbb82880dd
ingenarel <ingenarelitems@gmail.com>
2024-07-10 20:08:49 +0600

    FINALLY I FUCKING FIGURED OUT HOW TO MAKE IT WORK FUCK YEAH

--- lua/_plugins_/_language-helps_.lua
+++ lua/_plugins_/_language-helps_.lua
@@ -2,9 +2,18 @@ require("mason").setup()
 
 require("mason-lspconfig").setup {
     ensure_installed = {
-    "pyright"
+    "pyright", -- Python
+    "clangd", -- C
+    "lua_ls", --lua
+    "bashls", --bash
     },
 }
+
+local lspconfig = require("lspconfig")
+    lspconfig.pyright.setup{}
+    lspconfig.lua_ls.setup{}
+    lspconfig.clangd.setup{}
+    lspconfig.bashls.setup{}
 
 vim.g.coq_settings = {
 	auto_start = "shut-up"
