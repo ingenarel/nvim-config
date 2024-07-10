d18ed96a7536b1d10b78e51df803c17e7cbc9b72
ingenarel <ingenarelitems@gmail.com>
2024-07-11 03:02:24 +0600

    fixed the stupid undefined variable vim error from lua_ls

--- lua/_plugins_/_language-helps_.lua
+++ lua/_plugins_/_language-helps_.lua
@@ -6,16 +6,24 @@ require("mason-lspconfig").setup {
     "clangd", -- C
     "lua_ls", --lua
     "bashls", --bash
-    },
+    }
 }
 
 local lspconfig = require("lspconfig")
     lspconfig.pyright.setup{}
-    lspconfig.lua_ls.setup{}
+    lspconfig.lua_ls.setup {
+        settings = {
+            Lua = {
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true)
+                }
+            }
+        }
+    }
+    -- lspconfig.lua_ls.setup{}
     lspconfig.clangd.setup{}
     lspconfig.bashls.setup{}
 
 vim.g.coq_settings = {
 	auto_start = "shut-up"
 }
-
