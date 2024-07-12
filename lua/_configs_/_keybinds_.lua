d3ba97a301f2718944116d9ed0b8228b522f99c1
ingenarel <ingenarelitems@gmail.com>
2024-07-13 00:36:44 +0600

    set space to leader key and changed my keymaps.

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -1,8 +1,10 @@
-vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeToggle<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F3>', ':Telescope find_files<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F15>', ':Telescope<CR>', {noremap = true}) -- shift f3 is <F15>
-vim.api.nvim_set_keymap('n', '<F4>', ':LazyGit<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<F25>', ':help<CR>', {noremap = true}) -- ctrl f1 is <F25>
-vim.api.nvim_set_keymap('n', '<F1>', ':Dashboard<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', {noremap = true})
+vim.g.mapleader=" "
 
+vim.api.nvim_set_keymap('n', '<leader>d', ':Dashboard<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>tf', ':Telescope find_files<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>T', ':Telescope<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>g', ':LazyGit<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader><leader>', ':terminal<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true})
