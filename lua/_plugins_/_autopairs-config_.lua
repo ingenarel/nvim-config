bd7a6c9cd05227c0267f4e498e44639a6b66f3ff
ingenarel <ingenarelitems@gmail.com>
2024-07-18 13:52:47 +0600

    removed some commented out code.

--- lua/_plugins_/_autopairs-config_.lua
+++ lua/_plugins_/_autopairs-config_.lua
@@ -1,11 +1,3 @@
 require("nvim-autopairs").setup{
     disable_in_visualblock = true,
-    -- ignored_next_char = nil
     }
-
--- local Rule = require('nvim-autopairs.rule')
--- local npairs = require('nvim-autopairs')
--- npairs.add_rule(
---     Rule("<", ">")
--- )
-
