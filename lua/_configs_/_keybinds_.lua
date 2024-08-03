3299a869dbe66ff972740fb7531cce55542fbd1b
ingenarel <ingenarelitems@gmail.com>
2024-08-03 17:09:42 +0600

    added telescope keybinds

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -4,14 +4,17 @@ vim.g.mapleader=" "
 vim.api.nvim_set_keymap("n", ":", ";", {noremap=true})
 vim.api.nvim_set_keymap("n", ";", ":", {noremap=true})
 
-vim.api.nvim_set_keymap("n", "<leader>D",           ":Dashboard<CR>",                                                                                       {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>tf",          ":Telescope find_files<CR>",                                                                            {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>tof",         ":Telescope oldfiles<CR>",                                                                              {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>ts",          ":Telescope live_grep<CR>",                                                                             {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>T",           ":Telescope<CR>",                                                                                       {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>f",           ":FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>",    {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>g",           ":FloatermNew --width=0.9 --height=0.9 --title=LAZYGIT --titleposition=center lazygit<CR>",             {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader><leader>t",   ":FloatermNew --width=1.0 --height=0.9 --title=TERMINAL --titleposition=center<CR>",                    {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>D",           ":Dashboard<CR>", {noremap=true})
+-- Telescope
+    vim.api.nvim_set_keymap("n", "<leader>tf",          ":Telescope find_files<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>tof",         ":Telescope oldfiles<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>ts",          ":Telescope live_grep<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>tb",           ":Telescope buffers<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>T",           ":Telescope<CR>", {noremap=true})
+-- Telescope
+vim.api.nvim_set_keymap("n", "<leader>f",           ":FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>g",           ":FloatermNew --width=0.9 --height=0.9 --title=LAZYGIT --titleposition=center lazygit<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader><leader>t",   ":FloatermNew --width=1.0 --height=0.9 --title=TERMINAL --titleposition=center<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>d",           ":lua require('dapui').toggle()<CR>", {noremap=true})
 
 function ChangeDirectoryToCurrentAndShow()
