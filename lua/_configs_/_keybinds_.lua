8117cff480febd8e1207a10c7828697a126b5eec
ingenarel <ingenarelitems@gmail.com>
2024-09-22 20:27:02 +0600

    keymaps change
    changed the keymaps order, and added keymap for deleting matched
    parenthesis

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -1,35 +1,14 @@
-vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", {noremap=true})
-vim.g.mapleader=" "
-
-vim.api.nvim_set_keymap("n", ":", ";", {noremap=true})
-vim.api.nvim_set_keymap("n", ";", ":", {noremap=true})
-
-vim.api.nvim_set_keymap("n", "<leader>D",           ":Dashboard<CR>", {noremap=true})
--- Telescope
-    vim.api.nvim_set_keymap("n", "<leader>tf",  ":Telescope find_files<CR>", {noremap=true})
-    vim.api.nvim_set_keymap("n", "<leader>tof", ":Telescope oldfiles<CR>", {noremap=true})
-    vim.api.nvim_set_keymap("n", "<leader>ts",  ":Telescope live_grep<CR>", {noremap=true})
-    vim.api.nvim_set_keymap("n", "<leader>tb",  ":Telescope buffers<CR>", {noremap=true})
-    vim.api.nvim_set_keymap("n", "<leader>T",   ":Telescope<CR>", {noremap=true})
--- Telescope
-vim.api.nvim_set_keymap("n", "<leader>f",           ":FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>g",           ":FloatermNew --width=0.9 --height=0.9 --title=LAZYGIT --titleposition=center lazygit<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader><leader>t",   ":FloatermNew --width=1.0 --height=0.9 --title=TERMINAL --titleposition=center<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>d",           ":lua require('dapui').toggle()<CR>", {noremap=true})
-vim.api.nvim_set_keymap("n", "<leader>lw",          ":set wrap!<CR>", {noremap=true})
+function SaveAndSource()
+    vim.cmd("w")
+    vim.cmd("source%")
+end
+vim.api.nvim_set_keymap("n", "<leader>R", ":lua SaveAndSource()<CR>", {noremap=true})
 
 function ChangeDirectoryToCurrentAndShow()
     vim.cmd("cd %:h")
     vim.cmd("pwd")
 end
 vim.api.nvim_set_keymap("n", "<leader>cd", ":lua ChangeDirectoryToCurrentAndShow()<CR>", {noremap=true})
-
-
-function SaveAndSource()
-    vim.cmd("w")
-    vim.cmd("source%")
-end
-vim.api.nvim_set_keymap("n", "<leader>R", ":lua SaveAndSource()<CR>", {noremap=true})
 
 function UPDATEALLSHIT()
     vim.cmd("PlugUpgrade")
@@ -40,4 +19,31 @@ function UPDATEALLSHIT()
 end
 vim.api.nvim_set_keymap("n", "<F5>", ":lua UPDATEALLSHIT()<CR>", {noremap=true})
 
-vim.api.nvim_set_keymap("n", "<leader>w", ":lua require('nvim-window').pick()<CR>", {noremap=true})
+-- leader map
+    vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", {noremap=true})
+    vim.g.mapleader=" "
+-- leader map
+
+--remappping
+    vim.api.nvim_set_keymap("n", ":", ";", {noremap=true})
+    vim.api.nvim_set_keymap("n", ";", ":", {noremap=true})
+--remappping
+
+-- Telescope
+    vim.api.nvim_set_keymap("n", "<leader>tf",  ":Telescope find_files<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>tof", ":Telescope oldfiles<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>ts",  ":Telescope live_grep<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>tb",  ":Telescope buffers<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>T",   ":Telescope<CR>", {noremap=true})
+-- Telescope
+-- terminal
+    vim.api.nvim_set_keymap("n", "<leader>f",         ":FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader>g",         ":FloatermNew --width=0.9 --height=0.9 --title=LAZYGIT --titleposition=center lazygit<CR>", {noremap=true})
+    vim.api.nvim_set_keymap("n", "<leader><leader>t", ":FloatermNew --width=1.0 --height=0.9 --title=TERMINAL --titleposition=center<CR>", {noremap=true})
+-- terminal
+
+vim.api.nvim_set_keymap("n", "<leader>D",  ":Dashboard<CR>", {noremap=true}) -- opens the dashboard
+vim.api.nvim_set_keymap("n", "<leader>d",  ":lua require('dapui').toggle()<CR>", {noremap=true}) -- debug thing ui toggle
+vim.api.nvim_set_keymap("n", "<leader>lw", ":set wrap!<CR>", {noremap=true}) -- wrap on off (just for that window)
+vim.api.nvim_set_keymap("n", "<leader>w",  ":lua require('nvim-window').pick()<CR>", {noremap=true}) -- pick a window fast
+vim.api.nvim_set_keymap("n", "<leader>sx", "mx%x`xx", {noremap=true})
