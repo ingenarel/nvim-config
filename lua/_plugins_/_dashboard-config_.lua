52bb6bc626d376bf37bd423033021e677d5f54bd
ingenarel <ingenarelitems@gmail.com>
2024-09-25 00:23:53 +0600

    updated dashboard

--- lua/_plugins_/_dashboard-config_.lua
+++ lua/_plugins_/_dashboard-config_.lua
@@ -1,9 +1,37 @@
 require("dashboard").setup{
     theme = 'hyper',
     config = {
-           mru = {limit=30},
-           footer = {
-               "",
-           },
-       },
+        header ={},
+        footer = {},
+        mru = {limit=25},
+        packages = { enable = false},
+        project = { enable = true, limit = 10},
+        shortcut = {
+            {
+                desc = "Find",
+                action = "Telescope find_files",
+                key = "f"
+            },
+            {
+                desc = "Grep",
+                action = "Telescope live_grep",
+                key = "g"
+            },
+            {
+                desc = "Old Files",
+                action = "Telescope oldfiles",
+                key = "o"
+            },
+            {
+                desc = "Telescope",
+                action = "Telescope",
+                key = "t"
+            },
+            {
+                desc = "Update",
+                action = "<F5>",
+                key = "u"
+            },
+        }
+    },
 }
