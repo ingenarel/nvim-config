41e0630fcd8e36e37bb89b8ec9a2cecce58fdd65
ingenarel <ingenarelitems@gmail.com>
2024-09-24 19:43:09 +0600

    lsp rename go brrrrrrrrrrrrrrrrrrrrrr

--- lua/_configs_/_keybinds_.lua
+++ lua/_configs_/_keybinds_.lua
@@ -47,4 +47,5 @@ vim.api.nvim_set_keymap("n", "<leader>D",  ":Dashboard<CR>", {noremap=true}) -- 
 vim.api.nvim_set_keymap("n", "<leader>d",  ":lua require('dapui').toggle()<CR>", {noremap=true}) -- debug thing ui toggle
 vim.api.nvim_set_keymap("n", "<leader>lw", ":set wrap!<CR>", {noremap=true}) -- wrap on off (just for that window)
 vim.api.nvim_set_keymap("n", "<leader>w",  ":lua require('nvim-window').pick()<CR>", {noremap=true}) -- pick a window fast
-vim.api.nvim_set_keymap("n", "<leader>sx", "mx%x`xx", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>sx", "mx%x`xx", {noremap=true}) -- remove parenthesis
+vim.api.nvim_set_keymap("n", "<leader>lr", ":lua vim.lsp.buf.rename()<CR>", {noremap=true}) -- lsp rename go brrrrrr
