748eff6154032b80aacb8e6de8df87b883ac9617
ingenarel <ingenarelitems@gmail.com>
2025-01-05 23:31:40 +0600

    removed nvim dap python and added my own config for it

--- lua/_plugins_/_dap-setup_.lua
+++ lua/_plugins_/_dap-setup_.lua
@@ -4,10 +4,33 @@ return {
         "nvim-neotest/nvim-nio",
         {
             "mfussenegger/nvim-dap",
-            dependencies = { "mfussenegger/nvim-dap-python" },
             config = function()
                 local dap = require("dap")
-                require("dap-python").setup(vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python")
+                dap.adapters.python = function(cb, config)
+                    if config.request == "attach" then
+                        ---@diagnostic disable-next-line: undefined-field
+                        local port = (config.connect or config).port
+                        ---@diagnostic disable-next-line: undefined-field
+                        local host = (config.connect or config).host or "127.0.0.1"
+                        cb {
+                            type = "server",
+                            port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+                            host = host,
+                            options = {
+                                source_filetype = "python",
+                            },
+                        }
+                    else
+                        cb {
+                            type = "executable",
+                            command = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python",
+                            args = { "-m", "debugpy.adapter" },
+                            options = {
+                                source_filetype = "python",
+                            },
+                        }
+                    end
+                end
                 dap.adapters.codelldb = {
                     type = "server",
                     port = "${port}",
@@ -27,6 +50,15 @@ return {
                         end,
                         cwd = "${workspaceFolder}",
                         stopOnEntry = false,
+                    },
+                }
+                dap.configurations.python = {
+                    {
+                        type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+                        request = "launch",
+                        name = "Launch file",
+                        program = "${file}", -- This configuration will launch the current file if used.
+                        pythonPath = "python",
                     },
                 }
             end,
