7b1f4200926c7c37db1a4481584ea3ee20239896
ingenarel <ingenarelitems@gmail.com>
2025-02-08 23:50:11 +0600

    (colorscheme): change ibl rainbow colors

--- lua/_plugins_/_ibl-setup_.lua
+++ lua/_plugins_/_ibl-setup_.lua
@@ -10,17 +10,16 @@ return {
             "RainbowViolet",
             "RainbowCyan",
         }
-
         local hooks = require("ibl.hooks")
 
         hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ff0055" })
+            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#f4ef00" })
+            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#76c1ff" })
+            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#FF8F26" })
+            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#00FF3A" })
             vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#00ffc8" })
         end)
 
         require("ibl").setup { exclude = { filetypes = { "dashboard" } }, indent = { highlight = highlight } }
